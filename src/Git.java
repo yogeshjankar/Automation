b7ea01f31ffca7ca83928083ba9f7590b70d97dd
Yogesh Jankar <jankaryogesh2972@gmail.com>
2022-05-10 17:01:51 +0530

    2

--- src/Git.java
+++ src/Git.java
@@ -4,7 +4,6 @@ public class Git {
 	public static void main(String[] args) {
 		System.out.println("100");
 		
-		////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
 	}
 	//https://github.com/yogeshjankar/Automation.git
 	
