430dfefc81ba07693189947189fbe1597ad942d9
Yogesh Jankar <jankaryogesh2972@gmail.com>
2022-05-13 13:02:46 +0530

    13

---
+++ src/assignments/Bluestone.java
@@ -0,0 +1,30 @@
+package assignments;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Bluestone {
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(50,TimeUnit.SECONDS);
+		driver.get("https://www.bluestone.com");
+		driver.findElement(By.id("denyBtn")).click();
+		Thread.sleep(5000);
+		//WebElement target = driver.findElement(By.xpath("//iframe[@id='webengage-engagement-callback-frame']"));
+		driver.switchTo().frame("webengage-engagement-callback-frame");
+
+
+
+
+
+
+	}
+
+}
--- src/xpath/SeleniumApiDocsLink.java
+++ src/xpath/SeleniumApiDocsLink.java
@@ -1,4 +1,4 @@
-package assignments;
+package xpath;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
