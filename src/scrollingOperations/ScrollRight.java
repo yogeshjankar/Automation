00363a4771ed9bec27a0f36f320f6fcb6210f849
Yogesh Jankar <jankaryogesh2972@gmail.com>
2022-06-02 22:49:21 +0530

    0

--- src/scrollingOperations/ScrollRight.java
+++ src/scrollingOperations/ScrollRight.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class ScrollUp {
+public class ScrollRight {
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
